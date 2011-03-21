b9cb4418e7718af57f3d2849572929f44d55544f
jldupont <github@jldupont.com>
2011-03-21 19:26:39 -0400

    debugging...

--- src/com/systemical/android/eventor/Main.java
+++ src/com/systemical/android/eventor/Main.java
@@ -1,3 +1,8 @@
+/**
+ * @author jldupont
+ * 
+ *   http://developer.android.com/reference/android/app/Activity.html
+ */
 package com.systemical.android.eventor;
 
 import com.systemical.android.eventor.MainService;
@@ -5,6 +10,7 @@ import com.systemical.android.eventor.MainService;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main extends Activity {
 	
@@ -19,6 +25,28 @@ public class Main extends Activity {
         startService(new Intent(this, MainService.class));
     }
 
+    public void onStart(){
+    	Log.d(TAG, "onStart");
+    }
     
+    public void onResume() {
+    	Log.d(TAG, "onResume");
+    }
+    
+    public void onPause() {
+    	Log.d(TAG, "onPause");    	
+    }
+    
+    public void onStop() {
+    	Log.d(TAG, "onStop");    	
+    }
+    
+    public void onDestroy() {
+    	Log.d(TAG, "onDestroy");    	
+    }
+    
+    public void onRestart() {
+    	Log.d(TAG, "onRestart");    	
+    }
     
 }//
